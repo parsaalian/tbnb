ef64cfbb07e61b4a80e5ee17fc4e22eb1af403ae
parsa alian <parsaalian0@gmail.com>
2020-11-15 10:42:25 +0330

    removed plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,17 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
-const _ = require(`lodash`)
-const path = require(`path`)
-
 module.exports = {
     /* Your site config here */
-    plugins: [{
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `data`,
-            path: `${__dirname}/src/data/`,
-            ignore: [`**/\.*`], // ignore files starting with a dot
-        },
-    }, ],
+    plugins: [],
 }
